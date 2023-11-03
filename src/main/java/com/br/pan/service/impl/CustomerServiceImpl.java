2ef108dcc182df957339553de687d7158872cea1
enison <enisonoliveira@hotmail.com>
2023-11-03 09:23:00 -0300

    versao com cash

--- src/main/java/com/br/pan/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/br/pan/service/impl/CustomerServiceImpl.java
@@ -74,7 +74,7 @@ public class CustomerServiceImpl  implements CustomerService{
     *@see   public List<Customer> findAll()
     */
     @Override
-    @Cacheable(value = "_methods")
+    @Cacheable(value = "customerCache")
     public List<Customer> findAll() {
         logger.info("listando todos os clientes");
         return customerRepository.findAll();
